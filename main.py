08ce30f2135f83c9d94483491aa1a640087d629d
Dissable <59100563+Dissable@users.noreply.github.com>
2020-05-11 23:33:43 +0300

    Update main.py

--- main.py
+++ main.py
@@ -7,77 +7,87 @@ from telebot import types
 import random
 import os
 
-def get_anek():
+
+TKNg=str(os.environ.get('TKN'))
+bot = telebot.TeleBot(TKNg)
+
+global catdict
+catdict={}
+def get_catdict():
     page = random.randrange(1, 500, 1)
-    url1 = f'http://anekdotov.net/anekdot/family/index-page-{page}.html'
+    url1 = f'http://anekdotov.net'
     website = requests.get(url1)
     website.encoding = 'cp1251'
     website_url = website.text
     if len(str(website.text))>100:
-#    if True:
-        from bs4 import BeautifulSoup
         soup = BeautifulSoup(website_url, 'lxml')
-        lst=[i.text for i in soup.find_all('table', {'class':'maintbl'})[0].find_all('form', {'method':'post'})[0].find_all('p') if (len(i.text) > 10 and 'Ч И Т А Т Ь' not in i.text)]
+        lst=[url1+i.get('href') for i in soup.find_all('table', {'class':'menuanekdot'})[0].find_all('a', {'class':'menuanekdot'})]
+        lstnames=[i.text for i in soup.find_all('table', {'class':'menuanekdot'})[0].find_all('a', {'class':'menuanekdot'})]
+        catdict=dict(zip(lstnames, lst))
+        return(catdict)
+    else:
+        return('Ошибка получения категорий')
+
+def get_anek(url1):
+    website = requests.get(url1)
+    website.encoding = 'cp1251'
+    website_url = website.text
+    if len(str(website.text))>100:
+        soup = BeautifulSoup(website_url, 'lxml')
+        lst=[i.text for i in soup.find_all('table', {'class':'maintbl'})[0].find_all('div', {'class':'anekdot'})]
         if len(lst) > 0:
             anekn=random.randrange(0,len(lst),1)
             return(lst[anekn])
         else:
-            return('— Ты где был? — С собакой гулял. — У нас же нет собаки. — А мы с ней на улице познакомились.')
+            return('Парсер не смог найти анекдот')
     else:
-        return('anekdotov.net спалил парсер _')
+        return('Сайт не ответил')
 
-TKNg=str(os.environ.get('TKN'))
-bot = telebot.TeleBot(TKNg)
+
 @bot.message_handler(commands=['start'])
 def hello(message):
     markup = types.ReplyKeyboardMarkup()
     markup.row('Расскажи анекдот')
     markup.row('Кто на свете всех милее, всех румяней и белее?')
-    bot.send_message(message.chat.id, "Выберите вопрос", reply_markup=markup)
+    bot.send_message(message.chat.id, "Добрый день, я x-temp-bot2. \n"
+                                      "База данных  по температуре в процессе наполнения и настройки. \n"
+                                      "Чтобы занять время, пока хозяин допишет работу с базой и отрисовку графиков, \n"
+                                      "Я могу поразвлекать вас анекдотами с anekdotov.net, \n"
+                                      "Выберите вопрос", reply_markup=markup)
 
 
 @bot.message_handler(func=lambda message: message.text=='Кто на свете всех милее, всех румяней и белее?')
 def Xen(message):
-bot.send_message(message.chat.id, f'На свете всех милее и румяней и белее - {message.from.username}!!!')
+    bot.send_message(message.chat.id, f'На свете всех милее и румяней и белее - {str(message.from_user.first_name)}')
+
 
 @bot.message_handler(func=lambda message: message.text=='Расскажи анекдот')
+def getcats(message):
+    markup1 = types.ReplyKeyboardMarkup()
+    global catdict
+    catdict = get_catdict()
+    if type(catdict)==dict:
+        for i in list(get_catdict())[:-2]:
+            markup1.row('/'+i)
+        bot.send_message(message.chat.id, "Выберите категорию, или можно вернуться в начало "+'/start', reply_markup=markup1)
+    else:
+        bot.send_message(message.chat.id, 'Ошибка'+str(catdict))
+
+
+@bot.message_handler(func=lambda message: message.text in ['/'+i for i in catdict])
 def anek(message):
-    bot.send_message(message.chat.id, get_anek())
+    bot.send_message(message.chat.id, get_anek(catdict[message.text[1:]]) +
+                                        '\n-------------------------------------------------'+
+                                        '\n еще один, или вернуться в начало /start')
+
 
 @bot.message_handler(func=lambda message: message.text.lower().strip()!='start')
 def echo(message):
     bot.send_message(message.chat.id, message.text)
 
-
+print ('Bot has started')
 if __name__ == '__main__':
     #bot.polling(none_stop=True)
     bot.infinity_polling()
-
-def stat(tag=0):
-    '''
-    Достали статистику по короновирусу в pd.DataFrame
-    :param tag:
-    :return:
-    '''
-    url = 'https://www.worldometers.info/coronavirus/'
-    website_url = requests.get(url).text
-    soup = BeautifulSoup(website_url, 'lxml')
-    table = soup.find_all('table')[tag]
-    rows = table.find_all('tr')
-    field_list = []
-    for i in range(12):
-        col = []
-        col.append(rows[0].find_all('th')[i].get_text().strip())  # отдельно добавляем заголовок
-        for row in rows[1:]:  # начинаем со второго ряда таблицы, потому что 0 уже обработали выше
-            r = row.find_all('td')  # находим все теги td для строки таблицы
-            col.append(r[i].get_text().strip())  # сохраняем данные в наш список
-        field_list.append(col)
-    d = dict()
-    for i in range(12):
-        d[field_list[i][0]] = field_list[i][1:]
-
-    df = pd.DataFrame(d)
-    df = df.rename(columns={'Country,Other': 'Country', 'Serious,Critical': 'SeriousCritical'})
-    return df
 
 print ('Done')
