d3acbcc86b68c754bc6ea490529d454c068df708
Dissable <59100563+Dissable@users.noreply.github.com>
2020-06-18 17:53:14 +0300

    changed print

--- dbworker.py
+++ dbworker.py
@@ -9,7 +9,8 @@ class Postgres(object):
                 dbconn=f.read().split('\n')
                 connec=psycopg2.connect(dbname=dbconn[0], user=dbconn[1], password=dbconn[2], host=dbconn[3])
             local_start=True
-            print('local_start='+str(local_sta        return connec
+            print('local_start='+str(local_start))
+            return connec
         except:
             DATABASE_URL = os.environ['DATABASE_URL']
             connec = psycopg2.connect(DATABASE_URL, sslmode='require')
